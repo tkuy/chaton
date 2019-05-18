0c265e4cd942430f485fe1e343454af61311be73
tkuy1 <thierry.kuy@lunatech.fr>
2019-05-18 15:15:22 +0200

    Correction du state.

--- src/fr/upem/net/tcp/nonblocking/frame/FrameReader.java
+++ src/fr/upem/net/tcp/nonblocking/frame/FrameReader.java
@@ -67,7 +67,6 @@ public class FrameReader implements Reader {
 				result = (Frame) currentReader.get();
 				state=State.DONE;
 				currentReader.reset();
-				reset();
 				return ProcessStatus.DONE;
 			} else {
 				return ProcessStatus.REFILL;
